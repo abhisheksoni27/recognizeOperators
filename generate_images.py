ffa10911335725fb5e039bf01f14a32c103803df
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-05-05 19:27:49 +0530

    create plus image

--- generate_images.py
+++ generate_images.py
@@ -2,8 +2,8 @@ import cv2
 import os
 import numpy as np
 
-DEFAULT_HEIGHT = 28
-DEFAULT_WIDTH = 28
+DEFAULT_HEIGHT = 100
+DEFAULT_WIDTH = 100
 
 
 def create_blank_image(width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT):
@@ -13,7 +13,7 @@ def create_blank_image(width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT):
 
 def show_image(image):
     cv2.imshow("image", image)
-    cv2.waitKey(1000)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
@@ -21,18 +21,30 @@ def create_minus_image():
     # Create blank image
     blank_image = create_blank_image()
 
-    rect_width = int(DEFAULT_WIDTH/2)
-    rect_height = int(DEFAULT_HEIGHT/5)
-    final_rect_height = int(rect_height + 4 * rect_height)
-    final_rect_width = int(rect_width + 3)
+    start_height = int(DEFAULT_HEIGHT * 0.1)
+    end_height = int(DEFAULT_HEIGHT * 0.9)
+    rect_height = int(end_height - start_height)
 
-    blank_image[rect_height:final_rect_height, rect_width: final_rect_width] = 255 * \
-        np.ones((final_rect_height - rect_height, final_rect_width - rect_width))
+    start_width = int(DEFAULT_WIDTH * 0.5 - 1.5)
+    end_width = int(DEFAULT_WIDTH * 0.5 + 3)
+    rect_width = int(end_width - start_width)
+
+    blank_image[start_height:end_height, start_width: end_width] = 255 * \
+        np.ones((rect_height, rect_width))
 
     minus = np.rot90(blank_image)
+
+    minus = cv2.resize(minus, (28,28))
     return minus
 
 
+def create_plus_image():
+    minus = create_minus_image()
+    minus = np.rot90(minus)
+    minus2 = create_minus_image()
+    plus = np.bitwise_or(minus, minus2)
+    return plus
 
-minus = create_minus_image()
+
+minus = create_plus_image()
 show_image(minus)
